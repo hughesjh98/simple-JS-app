8c5837050bf9b9f0e6b77fe42f029f930c518cec
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2022-10-03 18:19:24 +0700

    add-pokemon-array
    delete practice JS and add a pokemon array.

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,7 @@
-alert('im back!!');
-
-let MyYear = 1998;
-let MyMonth = 'october';
-let MyDay = 30;
-let MyAge = 23;
-
-document.write(MyMonth);
-
+let pokemonList = [
+    {name: "Bulbasaur", type: ["grass", "posion"]},
+    {name: "Charizard", height:5, type: ["fire", "flying"]},
+    {name: "Mewtwo", height: 6, type: "psychic"},
+    {name:"Golurk", height:9, type: ["ground", "ghost"]},
+    {name: "Arceus", height: 10, type: "normal"}
+]
