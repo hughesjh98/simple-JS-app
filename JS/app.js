3db24272de24a4a0ff4583945e25a91240c8dbf3
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2022-10-03 20:15:24 +0700

    update-pokemonList-array

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: "Bulbasaur", type: ["grass", "posion"]},
-    {name: "Charizard", height:5, type: ["fire", "flying"]},
+    {name: "Bulbasaur", height: 2,type: ["grass", "posion"]},
+    {name: "Charizard", height: 5, type: ["fire", "flying"]},
     {name: "Mewtwo", height: 6, type: "psychic"},
-    {name:"Golurk", height:9, type: ["ground", "ghost"]},
+    {name:"Golurk", height: 9, type: ["ground", "ghost"]},
     {name: "Arceus", height: 10, type: "normal"}
 ]
