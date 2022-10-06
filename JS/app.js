03747d7298dc0263e1d514a7400536a76dc73beb
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2022-10-06 19:06:10 +0700

    add-for-loop
    add a for loop to find the correct pokemon and display content in the DOM.

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,49 @@
 let pokemonList = [
-    {name: "Bulbasaur", height: 2,type: ["grass", "posion"]},
-    {name: "Charizard", height: 5, type: ["fire", "flying"]},
-    {name: "Mewtwo", height: 6, type: "psychic"},
-    {name:"Golurk", height: 9, type: ["ground", "ghost"]},
-    {name: "Arceus", height: 10, type: "normal"}
+    {
+        name: "Bulbasaur", 
+        height: 2,
+        type: ["grass", "posion"]
+   },
+
+    {
+        name: "Charizard",
+        height: 5,
+        type: ["fire", "flying"]},
+
+    {
+        name: "Mewtwo",
+        height: 6,
+        type: "psychic"
+   },
+
+    {
+        name:"Golurk", 
+        height: 9, 
+        type: ["ground", "ghost"]
+   },
+
+    {
+        name: "Arceus", 
+        height: 10, 
+        type: "normal"
+   }
 ]
+
+
+// loop over and display pokemon with a specific height and display results in an ordered list.
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height === 2){
+      document.getElementById("pokemon1").innerHTML= pokemonList[i].name + " is a grass and poison pokemon.";
+
+    }else if (pokemonList[i].height === 5){
+        document.getElementById("pokemon2").innerHTML= pokemonList[i].name + " is a fire and flying pokemon.";
+
+    }else if (pokemonList[i].height === 6){
+        document.getElementById("pokemon3").innerHTML= pokemonList[i].name + " is an psychic pokemon.";
+
+    } else if (pokemonList[i].height === 9){
+      document.getElementById("pokemon4").innerHTML= pokemonList[i].name + " is a ground and ghost pokemon.";
+    } else{ 
+        document.getElementById("pokemon5").innerHTML= pokemonList[i].name + " is a normal pokemon.";
+    }
+  }
